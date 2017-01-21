ef70f03f4d870e7b0f7bac5b7fb72c2dde4d96e3
Ryan Shee <ryan.shee@gmail.com>
2017-01-21 09:15:04 -0800

    Initial commit

---
+++ app/src/main/java/com/hackucsc2017/underwater_squad/gotchobook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hackucsc2017.underwater_squad.gotchobook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
