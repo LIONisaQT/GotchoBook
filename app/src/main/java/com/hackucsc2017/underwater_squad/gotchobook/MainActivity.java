7ac64518c8a4100c4d9a39bffffa87e74e87e26d
LIONisaQT <ryan.shee@gmail.com>
2017-01-21 18:57:05 -0800

    Missing age on main page, sorta-final hard-coded version

--- app/src/main/java/com/hackucsc2017/underwater_squad/gotchobook/MainActivity.java
+++ app/src/main/java/com/hackucsc2017/underwater_squad/gotchobook/MainActivity.java
@@ -1,13 +1,33 @@
 package com.hackucsc2017.underwater_squad.gotchobook;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    GridView grid;
+    String[] names = {
+            "Austin",
+            "Austin",
+            "Austin",
+            "Austin"
+    };
+    int[] imageId = {
+            R.drawable.austin,
+            R.drawable.austin,
+            R.drawable.austin,
+            R.drawable.austin
+    };
 
-    public void goToAustin(View view) {
+    public void goToAustin() {
         Intent intent = new Intent(MainActivity.this, ChildView.class);
         startActivity(intent);
     }
@@ -16,5 +36,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomGrid adapter = new CustomGrid(MainActivity.this, names, imageId);
+        grid=(GridView)findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                goToAustin();
+            }
+        });
     }
 }
